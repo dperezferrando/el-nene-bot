6b2328488e615190312d43c45245d0656945e110
Diego <diegoperezferrando@gmail.com>
2021-06-25 22:37:38 -0300

    Update config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,14 @@
 module.exports = {
   discord: {
     botToken: process.env.BOT_TOKEN
+  },
+  frases: {
+    apiUrl: process.env.FRASES_API_URL || "http://decada.punchbangstuff.com/api",
+    session: process.env.API_SESSION,
+    maxFrases: process.env.MAX_FRASES || 437
+  },
+  polly: {
+    url: process.env.POLLY_URL || "https://streamlabs.com/polly",
+    voice: process.env.voice || "Penelope"
   }
 };
