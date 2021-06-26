99442fd44dbad4b83f7db2cbfa704a41a2168edd
Diego <diegoperezferrando@gmail.com>
2021-06-26 20:27:12 -0300

    Borro lo comentado

--- bot.js
+++ bot.js
@@ -16,45 +16,4 @@ const nene = new ElNene(client, commands);
 
 client.on('message', message => nene.processMessage(message));
 
-// client.on('message', async message => {
-
-//   if (!message.guild) return;
-
-//   if (_.toLower(message.content) === '/veninene') {
-
-//     if (message.member.voice.channel) {
-//       const ids = client.voice.connections.map(it => it.channel.id)
-//       if(_.includes(ids, message.member.voice.channel.id))
-//           return;
-//       const connection = await message.member.voice.channel.join();
-//       const audio = _.sample(audios);
-//       console.log("PLAYING", audio)
-//       const dispatcher = connection.play(`http://www.punchbangstuff.com/media/botonera/${audio}`);
-//       dispatcher.on("error", console.log)
-//       dispatcher.on("finish", () => connection.disconnect())
-//     } else {
-//       message.reply('Metete en un canal de voz, mogolico');
-//     }
-//   }
-
-//   if (_.toLower(message.content) === '/frase') {
-
-//     if (message.member.voice.channel) {
-//       const ids = client.voice.connections.map(it => it.channel.id)
-//       if(_.includes(ids, message.member.voice.channel.id))
-//           return;
-//       const frase = await frasesApi.randomFrase();
-//       console.log("FRASE:", JSON.stringify(frase))
-//       const linkDeLaPuta = await pollyChoreada.hablaPuta(`${frase.frase}. ${frase.autor} (${frase.anio})`)
-//       console.log(linkDeLaPuta)
-//       const connection = await message.member.voice.channel.join();
-//       const dispatcher = connection.play(linkDeLaPuta);
-//       dispatcher.on("error", console.log)
-//       dispatcher.on("finish", () => connection.disconnect())
-//     } else {
-//       message.reply('metete en un canal de voz, mogolico');
-//     }
-//   }
-// });
-
 client.on('error', (err) => console.log("EL NENE MURIO", err))
