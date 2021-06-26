4b52781173402e8decc2a710f7cc4c2ef0d85225
Diego <diegoperezferrando@gmail.com>
2021-06-26 19:46:48 -0300

    Termino voice Command

--- domain/commands/voiceCommand.js
+++ domain/commands/voiceCommand.js
@@ -8,8 +8,10 @@ class VoiceCommand {
     this.audioSource = audioSource;
   }
 
-  process(message) {
+  process(client, message) {
     // Se mezcla streams con promises, no se como evitarlo con estos eventos de mierda
+    if(!message.guild)
+      return Promise.resolve();
     if (!message.member.voice.channel) 
       return message.reply('Metete en un canal de voz, mogolico');
     const ids = client.voice.connections.map(it => it.channel.id)
@@ -20,6 +22,10 @@ class VoiceCommand {
       .then(({ connection, audio }) => ({ dispatcher: connection.play(audio), connection }))
       .then(({ connection, dispatcher}) => this._handleDispatcherEvents(connection, dispatcher))
 
+  }
+
+  isCommand(commandText) {
+    return `/${this.commandText}` == commandText;
   }
 
   _handleDispatcherEvents(connection, dispatcher) {
