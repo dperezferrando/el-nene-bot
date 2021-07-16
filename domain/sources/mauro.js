4584021dcce5afd59ee8510fca7a37279174be6e
Diego <diegoperezferrando@gmail.com>
2021-07-15 22:40:09 -0300

    Mauro

---
+++ domain/sources/mauro.js
@@ -0,0 +1,21 @@
+const Promise = require("bluebird");
+const _ = require("lodash")
+
+class Mauro {
+
+  get(client, channel) {
+    return Promise.resolve(channel.messages.fetch({ limit: 100 }))
+    .then(it => it.map(_.identity))
+    .filter(message => this._isValidMessage(message, client))
+    .then(_.sample)
+    .then(message => message ? message : { content: "Mauro dice: no vi nada capo"})
+    .get("content");
+  }
+
+  _isValidMessage(message, client) {
+    return !_.isEmpty(message.embeds) && message.author.id != client.user.id;
+  }
+
+}
+
+module.exports = Mauro;
