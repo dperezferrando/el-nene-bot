a6203bf2690dbfdd09f665bdf28de2b13acee808
Diego <diegoperezferrando@gmail.com>
2021-07-27 23:13:49 -0300

    Comando pacto

--- domain/commands/index.js
+++ domain/commands/index.js
@@ -4,10 +4,12 @@ const Botonera = require("../sources/botonera");
 const VoicedFrase = require("../sources/voicedFrase");
 const SimpleText = require("../sources/simpleText");
 const Mauro = require("../sources/mauro");
+const YouTube = require("../sources/youTube");
 
 module.exports = [
   new VoiceCommand("botonera", new Botonera()),
   new VoiceCommand("frase", new VoicedFrase()),
-  new TextCommand("help", new SimpleText("TOMÁ MOGOLICO: \n /botonera - /frase - /help - /mauro")),
+  new VoiceCommand("pacto", new YouTube("https://www.youtube.com/watch?v=P8u8md-NiHM")),
+  new TextCommand("help", new SimpleText("TOMÁ MOGOLICO: \n /botonera - /frase - /help - /mauro - /pacto")),
   new TextCommand("mauro", new Mauro())
 ];
